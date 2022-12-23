824774e089f435834fa83011a8145690a9876f60
Burak Kızılobek <76399335+BurakKizilobek@users.noreply.github.com>
2022-12-23 17:37:41 +0300

    Update expToScript.jsx

--- expToScript.jsx
+++ expToScript.jsx
@@ -1,4 +1,4 @@
-// Description: convert After Effects expressions for use in a JSX script as a string
+// Description: convert After Effects expressions for using in a JSX script as a string
 
 
 
@@ -13,15 +13,25 @@ var convertedExpArray = [];
 
 // convert lines of exp.txt file to array
 for (var i = 0; i < expFileContent.split("\n").length; i++) {
-    expFileContentArray[i] = expFileContent.split("\n")[i];
-    convertedExpArray[i] = "'" + expFileContentArray[i] + "\\n+'" ;
+    if (i != expFileContent.split("\n").length - 1) {
+        expFileContentArray[i] = expFileContent.split("\n")[i];
+        convertedExpArray[i] = "'" + expFileContentArray[i] + "\\n+'";
+    }
+    else {
+        expFileContentArray[i] = expFileContent.split("\n")[i];
+        convertedExpArray[i] = "'" + expFileContentArray[i] + "'";
+    }
 }
+
+
+
 
 expFileObj.close();
 
 
 // open exp.txt file with write mode and write convertedExpArray to exp.txt file
 expFileObj.open("w");
+
 var expFileContent = expFileObj.write(convertedExpArray.join("\n")); // write convertedExpArray to exp.txt file
 
 
